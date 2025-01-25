bdf28be49c31805d79fad58ddb232f504608de3f
TimmeaF <TLFlores4@MY.SCCCD.EDU>
2025-01-25 11:28:27 -0800

    Add files via upload

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+// Header file for input output fuctions
+#include <iostream>
+using namespace std;
+
+// main() function: where the execution off
+// C++ program begins
+int main() {
+
+    // This statement prints "Hello World"
+    cout << "Hello World!";
+
+    return 0;
+}
